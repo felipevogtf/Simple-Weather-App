892ae8ffeb8f42f3b854f1f4ea5d4e676566f95a
Felipe Vogt <felipevogt.f@gmail.com>
2023-02-27 11:30:13 -0300

    demo images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    StatusBar.setBackgroundColor({
+      color: '#000000',
+    }).then((data) => console.log(data));
+  }
 
   ngOnInit() {}
 }
